2677cbb38a5c7dcffee0788bde703dbb8aa4f8d1
samuelneo <24467645+samuelneo@users.noreply.github.com>
2025-03-09 22:26:28 +0800

    Move attribute package

--- src/main/java/seedu/address/model/attribute/Attribute.java
+++ src/main/java/seedu/address/model/attribute/Attribute.java
@@ -1,4 +1,4 @@
-package seedu.address.model.person.attribute;
+package seedu.address.model.attribute;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
