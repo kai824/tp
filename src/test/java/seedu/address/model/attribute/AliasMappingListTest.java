29cb00142769fddf9d2ab1007b318ef24e3eb8bb
h-b-k-nishi <hbk.homerun@gmail.com>
2025-03-23 23:18:24 +0800

    Add tests for AliasMappingList

---
+++ src/test/java/seedu/address/model/attribute/AliasMappingListTest.java
@@ -0,0 +1,36 @@
+package seedu.address.model.attribute;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+public class AliasMappingListTest {
+    @Test
+    void creation_defaultValue() {
+        AliasMappingList list = new AliasMappingList();
+        assertTrue(list.getAlias("github").isPresent());
+        assertTrue(list.getAlias("github").get().equals("https://github.com/"));
+        assertTrue(list.getAlias("LINKEDIN").isPresent());
+        assertTrue(list.getAlias("LINKEDIN").get().equals("https://www.linkedin.com/in/"));
+        assertFalse(list.getAlias("").isPresent());
+    }
+    @Test
+    void updateAlias_success() {
+        AliasMappingList list = new AliasMappingList();
+        String name1 = "jsi390209dsifnir";
+        String link1 = "/\\09438nfqJui43-0(*)";
+        list.updateAlias(name1, Optional.of(link1));
+        list.updateAlias("github", Optional.empty());
+        assertFalse(list.getAlias("github").isPresent());
+        assertTrue(list.getAlias("LINKEDIN").isPresent());
+        assertTrue(list.getAlias("LINKEDIN").get().equals("https://www.linkedin.com/in/"));
+        assertFalse(list.getAlias("").isPresent());
+        assertTrue(list.getAlias(name1).isPresent());
+        assertTrue(list.getAlias(name1.toUpperCase()).isPresent());
+        assertTrue(list.getAlias(name1).get().equals(link1));
+        assertTrue(list.getAlias(name1.toUpperCase()).get().equals(link1));
+    }
+}
