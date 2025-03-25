9d783b027880fe5d5fdf8730549a45a9f5a50888
Tan Yi Kai <yikai.tan.sg@gmail.com>
2025-03-25 21:48:35 +0800

    Implement Undo with a stack

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,10 +10,15 @@ public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
 
-    public static final String MESSAGE_UNDO_SUCCESS = "Undo success!";
+    public static final String MESSAGE_UNDO_SUCCESS = "Last command successfully undone!";
+
+    public static final String MESSAGE_NO_LAST_CHANGE = "There is no change to undo!";
 
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult(MESSAGE_UNDO_SUCCESS, false, false);
+    public CommandResult execute(Model model) throws CommandException {
+        if (!model.revertLastState()) {
+            throw new CommandException(MESSAGE_NO_LAST_CHANGE);
+        }
+        return new CommandResult(MESSAGE_UNDO_SUCCESS);
     }
 }
