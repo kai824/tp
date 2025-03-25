be9849055411eb644391176ae0baef74f366f651
Tan Yi Kai <yikai.tan.sg@gmail.com>
2025-03-25 23:25:48 +0800

    Remove unnecessary check
    Because of how ModelManager ignores equivalent previous states revertLastState, this check is no longer needed

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -10,7 +10,6 @@ import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -49,9 +48,7 @@ public class LogicManager implements Logic {
 
         CommandResult commandResult;
         Command command = addressBookParser.parseCommand(commandText);
-        if (!(command instanceof UndoCommand)) {
-            model.saveState();
-        }
+        model.saveState();
         commandResult = command.execute(model);
 
         try {
